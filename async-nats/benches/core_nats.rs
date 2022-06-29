10c7fc37f6b2b7165c57de645c4282ec30989f6b
Tomasz Pietrek <tomasz@nats.io>
2022-06-29 12:23:36 +0200

    Add Jetstream benchmarks

--- async-nats/benches/core_nats.rs
+++ async-nats/benches/core_nats.rs
@@ -1,11 +1,12 @@
+use async_nats::jetstream::{self, consumer::PullConsumer};
+use bytes::Bytes;
 use criterion::{criterion_group, criterion_main, Criterion};
-use futures::stream::StreamExt;
+use futures::{stream::StreamExt, TryStreamExt};
 
 pub fn publish(c: &mut Criterion) {
     let server = nats_server::run_basic_server();
     let mut throughput_group = c.benchmark_group("async-nats: publish throughput");
     throughput_group.sample_size(10);
-    throughput_group.warm_up_time(std::time::Duration::from_secs(1));
 
     let bmsg: Vec<u8> = (0..32768).map(|_| 22).collect();
     for size in [32, 128, 256, 1024, 4096, 8192].iter() {
@@ -30,7 +31,6 @@ pub fn publish(c: &mut Criterion) {
 
     let mut messages_group = c.benchmark_group("async-nats: publish messages amount");
     messages_group.sample_size(10);
-    messages_group.warm_up_time(std::time::Duration::from_secs(1));
 
     let bmsg: Vec<u8> = (0..32768).map(|_| 22).collect();
     for size in [32, 128, 256, 1024, 4096, 8192].iter() {
@@ -63,7 +63,6 @@ pub fn subscribe(c: &mut Criterion) {
 
     let mut subscribe_amount_group = c.benchmark_group("subscribe amount");
     subscribe_amount_group.sample_size(10);
-    subscribe_amount_group.warm_up_time(std::time::Duration::from_secs(1));
 
     for size in [32, 128, 256, 1024, 4096, 8192].iter() {
         subscribe_amount_group.throughput(criterion::Throughput::Elements(1000));
@@ -102,6 +101,139 @@ pub fn subscribe(c: &mut Criterion) {
     }
     subscribe_amount_group.finish();
 }
+pub fn pull_consumer_stream(c: &mut Criterion) {
+    let server = nats_server::run_server("tests/configs/jetstream.conf");
+
+    let mut pull_consumer_group = c.benchmark_group("pull_stream");
+
+    for size in [32, 128, 256, 1024, 4096, 8192].iter() {
+        pull_consumer_group.throughput(criterion::Throughput::Elements(1000));
+        pull_consumer_group.bench_with_input(
+            criterion::BenchmarkId::from_parameter(size),
+            size,
+            |b, _| {
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                let nc = rt.block_on(async {
+                    let nc = async_nats::connect(server.client_url()).await.unwrap();
+
+                    tokio::task::spawn({
+                        let nc = nc.clone();
+                        async move {
+                            let bmsg: Vec<u8> = (0..32768).map(|_| 22).collect();
+                            let msg = &bmsg[0..*size].to_vec();
+
+                            loop {
+                                nc.publish("bench".to_string(), msg.clone().into())
+                                    .await
+                                    .unwrap();
+                            }
+                        }
+                    });
+                    nc.flush().await.unwrap();
+                    nc
+                });
+
+                b.to_async(rt).iter(move || {
+                    let nc = nc.clone();
+                    async move {
+                        let jetstream = jetstream::new(nc.clone());
+                        let stream = jetstream
+                            .create_stream(jetstream::stream::Config {
+                                name: "pull".to_string(),
+                                subjects: vec!["bench".to_string()],
+                                ..Default::default()
+                            })
+                            .await
+                            .unwrap();
+                        let consumer: PullConsumer = stream
+                            .get_or_create_consumer(
+                                "pull_consumer",
+                                jetstream::consumer::pull::Config {
+                                    durable_name: Some("pull_consumer".to_string()),
+                                    ..Default::default()
+                                },
+                            )
+                            .await
+                            .unwrap();
+                        let mut messages = consumer.stream().await.unwrap().take(1000);
+                        while let Some(result) = messages.next().await {
+                            result.unwrap().ack().await.unwrap();
+                        }
+                    }
+                });
+            },
+        );
+    }
+    pull_consumer_group.finish();
+}
+
+pub fn pull_consumer_sequence(c: &mut Criterion) {
+    let server = nats_server::run_server("tests/configs/jetstream.conf");
+
+    let mut pull_consumer_group = c.benchmark_group("pull_sequence");
+
+    for size in [32, 128, 256, 1024, 4096, 8192].iter() {
+        pull_consumer_group.throughput(criterion::Throughput::Elements(1000));
+        pull_consumer_group.bench_with_input(
+            criterion::BenchmarkId::from_parameter(size),
+            size,
+            |b, _| {
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                let nc = rt.block_on(async {
+                    let nc = async_nats::connect(server.client_url()).await.unwrap();
+
+                    tokio::task::spawn({
+                        let nc = nc.clone();
+                        async move {
+                            let bmsg: Vec<u8> = (0..32768).map(|_| 22).collect();
+                            let msg = &bmsg[0..*size].to_vec();
+
+                            loop {
+                                nc.publish("bench".to_string(), msg.clone().into())
+                                    .await
+                                    .unwrap();
+                            }
+                        }
+                    });
+                    nc.publish("data".to_string(), "data".into()).await.unwrap();
+                    nc.flush().await.unwrap();
+                    nc
+                });
+
+                b.to_async(rt).iter(move || {
+                    let nc = nc.clone();
+                    async move {
+                        let jetstream = jetstream::new(nc.clone());
+                        let stream = jetstream
+                            .create_stream(jetstream::stream::Config {
+                                name: "pull".to_string(),
+                                subjects: vec!["bench".to_string()],
+                                ..Default::default()
+                            })
+                            .await
+                            .unwrap();
+                        let consumer: PullConsumer = stream
+                            .get_or_create_consumer(
+                                "pull_consumer",
+                                jetstream::consumer::pull::Config {
+                                    durable_name: Some("pull_consumer".to_string()),
+                                    ..Default::default()
+                                },
+                            )
+                            .await
+                            .unwrap();
+                        let mut iter = consumer.sequence(100).unwrap().take(1000);
+                        while let Ok(Some(mut batch)) = iter.try_next().await {
+                            while let Ok(Some(_)) = batch.try_next().await {}
+                        }
+                    }
+                });
+            },
+        );
+    }
+    pull_consumer_group.finish();
+}
+
 async fn publish_1000_messages(nc: async_nats::Client, msg: &'_ [u8]) {
     let msg = msg.to_vec();
     for _i in 0..1000 {
@@ -119,5 +251,11 @@ async fn subscribe_1000_messages(nc: async_nats::Client) {
     }
 }
 
-criterion_group!(benches, publish, subscribe);
+criterion_group!(
+    benches,
+    pull_consumer_stream,
+    pull_consumer_sequence,
+    publish,
+    subscribe
+);
 criterion_main!(benches);
