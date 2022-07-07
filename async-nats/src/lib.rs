ffb74e8fab7e4ec75f4716293da547a18cc85e49
Paul Butler <paul@driftingin.space>
2022-07-07 10:48:51 +0000

    Add 'static bound to Error type

--- async-nats/src/lib.rs
+++ async-nats/src/lib.rs
@@ -124,7 +124,7 @@ use tokio::net::TcpStream;
 use tokio::sync::{mpsc, oneshot};
 use tokio::task;
 
-pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const LANG: &str = "rust";
